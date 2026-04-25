f80308255a6aefd164c4c460ec0b00df7cfd891b
Ibrohimxon Mahmudxonov <mhdvmim@gmail.com>
2026-04-25 05:08:36 -0700

    full

---
+++ app.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+});
+
+
+
+
